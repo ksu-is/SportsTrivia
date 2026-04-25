859ab1055f21ae90752efd29b67a036b89da4d85
TylerGeorge28 <tgeorg28@students.kennesaw.edu>
2026-04-24 22:15:22 -0400

    Update SportsTrivia.py
    Added a way for the scores to be saved and a high score

--- SportsTrivia.py
+++ SportsTrivia.py
@@ -12,3 +12,29 @@ def ask_question(question, options, correct_answer):
     else:
         print("Wrong!")
         return 0
+def save_score(name, score):
+    file = open("scores.txt", "a")
+    file.write(f"{name},{score}\n")
+    file.close()
+
+def show_high_score():
+    try:
+        file = open("scores.txt", "r")
+        high_score = 0
+        top_player = ""
+
+        for line in file:
+            data = line.strip().split(",")
+            score = int(data[1])
+
+            if score > high_score:
+                high_score = score
+                top_player = data[0]
+
+        file.close()
+
+        if top_player != "":
+            print(f"\nHigh Score: {top_player} with {high_score}")
+    except:
+        print("No scores yet.")
+
